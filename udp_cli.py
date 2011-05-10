7731332b5aef8de2b4d132f51e142f8334f3bc72
Andrey Smirnov <smira@qik.com>
2011-05-10 15:00:47 +0400

    UDP test case.

---
+++ udp_cli.py
@@ -0,0 +1,35 @@
+#!env/bin/python
+
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor, task
+
+class Helloer(DatagramProtocol):
+
+    def startProtocol(self):
+        host = "10.3.2.95"
+        port = 1234
+
+        self.n = 0
+
+        self.transport.connect(host, port)
+        print "now we can only send to host %s port %d" % (host, port)
+        self.transport.write("hello") # no need for address
+        task.LoopingCall(self.pinger).start(1)
+
+    def pinger(self):
+        self.n += 1
+        self.transport.write("%d\n" % self.n)
+        print "sent %d" % self.n
+
+    def datagramReceived(self, data, (host, port)):
+        print "received %r from %s:%d" % (data, host, port)
+
+    # Possibly invoked if there is no server listening on the
+    # address to which we are sending.
+    def connectionRefused(self):
+        print "No one listening"
+
+# 0 means any port, we don't care in this case
+reactor.listenUDP(0, Helloer())
+reactor.run()
+
