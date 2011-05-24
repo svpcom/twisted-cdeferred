2f1e9f2728822f4095f46d0356317acf097c9aca
Vasiliy Evseenko <vasiliy.evseenko@qik.com>
2011-05-24 09:50:50 +0400

    Disable twisted/test/raiser.c for PyPy compatibility

--- twisted/topfiles/setup.py
+++ twisted/topfiles/setup.py
@@ -23,7 +23,8 @@ from twisted.python.dist import twisted_subprojects
 
 extensions = [
     Extension("twisted.test.raiser",
-              ["twisted/test/raiser.c"]),
+              ["twisted/test/raiser.c"],
+               condition=lambda builder: sys.subversion[0] != "PyPy"),
 
     Extension('twisted.internet.cdefer',
               ['twisted/internet/cdefer/cdefer.c']),
